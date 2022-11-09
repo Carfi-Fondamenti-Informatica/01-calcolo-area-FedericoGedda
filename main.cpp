4eb07e7f4818c7f47fffc439c6dfa519b9d7fbec
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-09 15:54:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+float a,b,c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
+cout<<"Area triangolo="<< (a*b)/2 <<endl;
+cout<<"Area quadrato="<< pow(a,2) << endl;
+cout<<"Area rettangolo="<< (a*b) <<endl;
+cout<<"Area trapezio="<< ((a+b)*c)/2 <<endl;
 
 }
