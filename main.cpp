08b3729b629d4b7325dbbaede327b1bd092565e3
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-09 15:57:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 cout<<"Area triangolo="<< (a*b)/2 <<endl;
-cout<<"Area quadrato="<< pow(a,2) << endl;
+cout<<"Area quadrato="<< a*a << endl;
 cout<<"Area rettangolo="<< (a*b) <<endl;
 cout<<"Area trapezio="<< ((a+b)*c)/2 <<endl;
 
