2a390306110d23ef37348080bb04f609a03eb512
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-09 16:03:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
  
-cout<<"Area triangolo="<< (a*b)/2 <<endl;
-cout<<"Area quadrato="<< a*a << endl;
-cout<<"Area rettangolo="<< (a*b) <<endl;
-cout<<"Area trapezio="<< ((a+b)*c)/2 <<endl;
+cout<<(a*b)/2 <<endl;
+cout<<a*a << endl;
+cout<<(a*b) <<endl;
+cout<<((a+b)*c)/2 <<endl;
   
 
 return 0;
